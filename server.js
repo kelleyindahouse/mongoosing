85e202c64fcecd3b43a348db7c73f2efe70f6990
Kevin K <kelleyindahouse@gmail.com>
2022-01-10 14:13:57 -0500

    cats and birds, yo yo

--- server.js
+++ server.js
@@ -8,7 +8,26 @@ const DogSchema = new Schema({
   breed: String
 })
 
+const CatSchema = new Schema({
+  name: String,
+  age: Number,
+  breed: String,
+  living: Boolean
+})
+
+const BirdSchema = new Schema ({
+  name: String,
+  age: Number,
+  breed: String,
+  living: Boolean,
+  canFly: Boolean
+})
+
 const Dog = model('dog', DogSchema)
+
+const Cat = model('cat', CatSchema)
+
+const Bird = model('bird', BirdSchema)
 
 Dog.create({
   name: 'Beef',
@@ -16,4 +35,23 @@ Dog.create({
   breed: 'Chihuahua'
 })
   .then(dog => console.log(dog))
+  .catch(err => console.log(err))
+
+Cat.create({
+  name: 'Cheese',
+  age: 3,
+  breed: 'Toiger',
+  living: true
+})
+  .then(cat => console.log(cat))
+  .catch(err => console.log(err))
+
+Bird.create({
+  name: 'Pengy',
+  age: 5,
+  breed: 'Penguin',
+  living: false,
+  canFly: false
+})
+  .then(bird => console.log(bird))
   .catch(err => console.log(err))
