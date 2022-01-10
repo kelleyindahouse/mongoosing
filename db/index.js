f743990dc7b2c1547b8ba8663a05aaff00a09d73
Kevin K <kelleyindahouse@gmail.com>
2022-01-10 14:59:27 -0500

    codin yo

--- db/index.js
+++ db/index.js
@@ -1,1 +1,1 @@
-modeule.exports = require('mongoose').connect('mongodb://localhost:27017/animals_db')
+module.exports = require('mongoose').connect('mongodb://localhost:27017/animals_db')
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 
 router.use('/api', require('./dogRoutes.js'))
+router.use('/api', require('./catRoutes.js'))
+router.use('/api', require('./birdRoutes.js'))
 
 module.exports = router
